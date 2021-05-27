83554f201bdbce49bc93703854069cd05ef45855
Lucas Emanuel <lucaxemanuel@gmail.com>
2021-05-27 19:43:03 -0300

    :white_check_mark: ensure TaskEntity throw an error when trying check a task already checked

--- src/domain/entities/task-entity.spec.js
+++ src/domain/entities/task-entity.spec.js
@@ -5,6 +5,7 @@ class TaskEntity {
   }
 
   checked () {
+    if (this.isChecked) throw new Error('The task is already checked.')
     this.isChecked = true
   }
 
@@ -15,11 +16,20 @@ class TaskEntity {
 
 const makeTaskEntity = () => new TaskEntity({ description: 'any_task' })
 
+const makeTaskEntityChecked = () =>
+  new TaskEntity({ description: 'any_task', isChecked: true })
+
 describe('Task Entity', () => {
   test('should check a task', () => {
     const sut = makeTaskEntity()
     sut.checked()
     expect(sut.isChecked).toBe(true)
+  })
+  test('should throw an error when trying check a task already checked', () => {
+    const sut = makeTaskEntityChecked()
+    expect(() => {
+      sut.checked()
+    }).toThrow()
   })
   test('should uncheck a task', () => {
     const sut = makeTaskEntity()
