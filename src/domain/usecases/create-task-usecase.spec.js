498a05fb6f87e5e84e66d1690a2291c648148870
Lucas Emanuel <lucaxemanuel@gmail.com>
2021-06-30 09:54:32 -0300

    :white_check_mark: ensure throw an error if TaskRepository is invalid

--- src/domain/usecases/create-task-usecase.spec.js
+++ src/domain/usecases/create-task-usecase.spec.js
@@ -1,15 +1,52 @@
 const { MissingParamError } = require('../../utils/errors')
 
+class InvalidParamError extends Error {
+  constructor (paramName) {
+    super(`Invalid param: ${paramName}`)
+    this.name = 'InvalidParamError'
+  }
+}
+
 class CreateTaskUseCase {
+  constructor ({ taskRepository } = {}) {
+    this.taskRepository = taskRepository
+  }
+
+  taskRepositoryIsValid () {
+    if (!this.taskRepository || !this.taskRepository.insert) {
+      throw new InvalidParamError('taskRepository')
+    }
+  }
+
   async execute (description) {
     if (!description) throw new MissingParamError('description')
+    this.taskRepositoryIsValid()
   }
+}
+
+class TaskRepository {
+  insert () {}
+}
+
+const makeSut = () => {
+  const taskRepository = new TaskRepository()
+  return new CreateTaskUseCase({ taskRepository })
 }
 
 describe('Check Task Use Case', () => {
   test('should throw if no description is provided', () => {
-    const createTaskUseCase = new CreateTaskUseCase()
-    const sut = createTaskUseCase.execute()
-    expect(sut).rejects.toThrow(new MissingParamError('description'))
+    const sut = makeSut()
+    const promise = sut.execute()
+    expect(promise).rejects.toThrow(new MissingParamError('description'))
+  })
+  test('should throw an error if TaskRepository is invalid', () => {
+    const suts = [
+      new CreateTaskUseCase(),
+      new CreateTaskUseCase({ taskRepository: {} })
+    ]
+    for (const sut of suts) {
+      const promise = sut.execute('any description')
+      expect(promise).rejects.toThrow(new InvalidParamError('taskRepository'))
+    }
   })
 })
