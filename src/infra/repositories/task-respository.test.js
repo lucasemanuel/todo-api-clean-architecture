a115c986db687b10704131f67fd2b8249828071a
Lucas Emanuel <lucaxemanuel@gmail.com>
2021-07-01 17:08:23 -0300

    :sparkles: ensure insert a task in database by create method

--- src/infra/repositories/task-respository.test.js
+++ src/infra/repositories/task-respository.test.js
@@ -1,17 +1,40 @@
+const { MongoClient } = require('mongodb')
+const { mongoUrl } = require('../config/database')
 const InvalidParamError = require('../../utils/errors/invalid-param-error')
 
+let connection
+let db
+
 class TaskRepository {
-  async create ({ description }) {
+  async create ({ description, is_checked = false }) {
     if (!description) throw new InvalidParamError('description')
+    const document = { description, is_checked }
+    await db.collection('tasks').insertOne(document)
   }
 }
 
 describe('Task Respository', () => {
+  beforeAll(async () => {
+    connection = await MongoClient.connect(mongoUrl, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    })
+    db = await connection.db()
+  })
+  afterAll(async () => {
+    await connection.close()
+  })
   test('should throw error if a description is invalid or no provided in method create', () => {
     const sut = new TaskRepository()
     const suts = [sut.create({}), sut.create({ description: '' })]
     for (const promise of suts) {
       expect(promise).rejects.toThrow(new InvalidParamError('description'))
     }
+  })
+  test('should insert a task in database by create method', async () => {
+    const sut = new TaskRepository()
+    await sut.create({ description: 'any description' })
+    const countTasks = await db.collection('tasks').countDocuments()
+    expect(countTasks).toBe(1)
   })
 })
