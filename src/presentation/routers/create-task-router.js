19d3ab5d7164e38af397154686d6251e6f59885e
Lucas Emanuel <lucaxemanuel@gmail.com>
2021-07-02 16:16:08 -0300

    :white_check_mark: ensure call CreateTaskUseCase with correct params

--- src/presentation/routers/create-task-router.js
+++ src/presentation/routers/create-task-router.js
@@ -1,16 +1,23 @@
 const HttpResponse = require('../helpers/http-response')
 const { MissingParamError } = require('../../utils/errors')
 
-module.exports = class CreateTaskRouter {
+class CreateTaskRouter {
+  constructor ({ createTaskUseCase }) {
+    this.createTaskUseCase = createTaskUseCase
+  }
+
   async route (httpRequest) {
     try {
-      const { description } = httpRequest.body
-
+      const { description, isChecked } = httpRequest.body
       if (!description) {
         return HttpResponse.badRequest(new MissingParamError('description'))
       }
+
+      await this.createTaskUseCase.execute({ description, isChecked })
     } catch (error) {
       return HttpResponse.serverError()
     }
   }
 }
+
+module.exports = CreateTaskRouter
--- src/presentation/routers/create-task-router.spec.js
+++ src/presentation/routers/create-task-router.spec.js
@@ -2,10 +2,19 @@ const CreateTaskRouter = require('./create-task-router')
 const { ServerError } = require('../errors')
 const { MissingParamError } = require('../../utils/errors')
 
+class CreateTaskUseCaseSpy {
+  async execute ({ description, isChecked }) {
+    this.description = description
+    this.isChecked = isChecked
+  }
+}
+
 const makeSut = () => {
-  const createTaskRouter = new CreateTaskRouter()
+  const createTaskUseCaseSpy = new CreateTaskUseCaseSpy()
+  const sut = new CreateTaskRouter({ createTaskUseCase: createTaskUseCaseSpy })
   return {
-    sut: createTaskRouter
+    sut,
+    createTaskUseCaseSpy
   }
 }
 
@@ -33,5 +42,17 @@ describe('Create Task Router', () => {
     const httpResponse = await sut.route(httpRequest)
     expect(httpResponse.statusCode).toBe(500)
     expect(httpResponse.body.error).toEqual(new ServerError())
+  })
+  test('should call CreateTaskUseCase with correct params', () => {
+    const { sut, createTaskUseCaseSpy } = makeSut()
+    const httpRequest = {
+      body: {
+        description: 'any description',
+        isChecked: false
+      }
+    }
+    sut.route(httpRequest)
+    expect(createTaskUseCaseSpy.description).toBe(httpRequest.body.description)
+    expect(createTaskUseCaseSpy.isChecked).toBe(httpRequest.body.isChecked)
   })
 })
