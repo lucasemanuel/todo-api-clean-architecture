b518483aa3bfc16b7d8948285e8f45a5a02df036
Lucas Emanuel <lucaxemanuel@gmail.com>
2021-07-05 16:09:35 -0300

    :white_check_mark: ensure throw error if TaskRepository is invalid

--- src/domain/usecases/delete-task-usecase.spec.js
+++ src/domain/usecases/delete-task-usecase.spec.js
@@ -1,8 +1,14 @@
-const { MissingParamError } = require('../../utils/errors')
+const { MissingParamError, InvalidParamError } = require('../../utils/errors')
 
 class DeleteTaskUseCase {
   async execute () {
     throw new MissingParamError('id')
+  }
+
+  taskRepositoryIsValid () {
+    if (!this.taskRepository || !this.taskRepository.delete) {
+      throw new InvalidParamError('taskRepository')
+    }
   }
 }
 
@@ -18,5 +24,17 @@ describe('Delete Task Use Case', () => {
     const { sut } = makeSut()
     const promise = sut.execute()
     expect(promise).rejects.toThrow(new MissingParamError('id'))
+  })
+  test('should throw error if TaskRepository is invalid', () => {
+    const suts = [
+      new DeleteTaskUseCase(),
+      new DeleteTaskUseCase({}),
+      new DeleteTaskUseCase({ taskRepository: {} })
+    ]
+    for (const sut of suts) {
+      expect(() => {
+        sut.taskRepositoryIsValid()
+      }).toThrow(new InvalidParamError('taskRepository'))
+    }
   })
 })
