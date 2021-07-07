c26e2e42d6f558c4c282c0e9b267a7413a64b6e1
Lucas Emanuel <lucaxemanuel@gmail.com>
2021-07-07 15:27:08 -0300

    :sparkles: add request params in adapter

--- src/infra/web/adapters/ExpressRouterAdapter.js
+++ src/infra/web/adapters/ExpressRouterAdapter.js
@@ -2,7 +2,8 @@ module.exports = {
   adapt (router) {
     return async (request, response) => {
       const httpRequest = {
-        body: request.body || {}
+        body: request.body || {},
+        params: request.params || {}
       }
       const httpResponse = await router.route(httpRequest)
       response.status(httpResponse.statusCode).json(httpResponse.body)
